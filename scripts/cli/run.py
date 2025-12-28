9a093cadbfd11eecd8063bb107183db2e79b04da
HungphamLeo <hungphamtudo@gmail.com>
2025-12-28 10:26:05 +0700

    update datalake schedule

--- scripts/cli/run.py
+++ scripts/cli/run.py
@@ -1,7 +1,7 @@
-# from scripts.run_wbapi_etl import run_pipeline
+from scripts.cophieu68_etl_mongodb_v1 import run_etl_cophieu69_mongodb_v1
 
 
-# if __name__ == "__main__":
-#     run_pipeline()
-#     print("Pipeline execution completed successfully.")
+if __name__ == "__main__":
+    run_etl_cophieu69_mongodb_v1()
+    print("Pipeline execution completed successfully.")
     
